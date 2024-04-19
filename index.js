c054c8372772389642ce363cc0361f7ae28e63c8
Laura <chirchir0814@gmail.com>
2024-04-19 16:04:31 +0300

    update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
-
+//functio
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
